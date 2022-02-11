1c799b2704a108693b952670eaf6616d135154f7
Alexander Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2022-02-12 01:44:48 +0200

    clean template

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div
+      className="App"
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        textTransform: 'uppercase',
+        color: '#010101',
+      }}
+    >
+      Wallet app
+    </div>
+  );
+}
+
+export default App;
