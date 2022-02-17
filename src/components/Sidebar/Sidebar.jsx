8ce46399e27c31abb4be8d0fa7517e578923b8e0
Alexander Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2022-02-18 00:31:18 +0200

    save

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,15 @@
 import Currency from '../Currency';
 import Navigation from '../Navigation/Navigation';
+import Media from 'react-media';
 import s from './Sidebar.module.css';
 
 export default function Sidebar() {
   return (
     <div className={s.wrapper}>
-      <Navigation></Navigation>
-      <Currency></Currency>
+      <div>
+        <Navigation></Navigation>
+      </div>
+      <Media query="(min-width: 768px)" render={() => <Currency></Currency>} />
     </div>
   );
 }
