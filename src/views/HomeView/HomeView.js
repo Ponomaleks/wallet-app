fdfb6ebd7f28ff4f276fefdaddfa80b579ff602b
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-02-18 13:07:39 +0200

    commit home fix

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,44 +1,48 @@
 import React, { Fragment } from 'react';
 import Media from 'react-media';
-import { Box, Container, Flexbox } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Box } from '@material-ui/core';
 import {
   TableTransactions,
   TableTransactionsMobile,
 } from '../../components/TableTransactions';
-// import Currency from '../components/Currency';
-// import DiagramTab from '../../components/DiagramTab/DiagramTab';
 import Sidebar from '../../components/Sidebar';
 import s from './HomeView.module.css';
 
 // import { useSelector } from 'react-redux';
 // import { getIsLoggedIn } from '../redux/user';
 
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     flexGrow: 1,
-//     padding: 0,
-//   },
-//   paper: {
-//     padding: theme.spacing(2),
-//     textAlign: 'center',
-//     color: theme.palette.text.secondary,
-//   },
-// }));
-
 const HomeView = () => {
-  // const classes = useStyles();
   // const isLoggedIn = useSelector(getIsLoggedIn);
 
   return (
     <div className={`container ${s.homeView} ${s.homeBackground}`}>
       {/* {!isLoggedIn && <div>Please, register or log in to start. </div>} */}
       <Sidebar />
-      <TableTransactions />
+      <Media
+        queries={{
+          mobile: '(max-width: 767px)',
+          other: '(min-width: 768px)',
+        }}
+      >
+        {matches => (
+          <Fragment>
+            {matches.mobile && (
+              <Box
+                height="100vh"
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                padding="0px"
+              >
+                <TableTransactionsMobile />
+              </Box>
+            )}
+            {matches.other && <TableTransactions />}
+          </Fragment>
+        )}
+      </Media>
     </div>
   );
 };
 
 export default HomeView;
-
-// <Divider className={classes.divider} orientation="vertical" />;
