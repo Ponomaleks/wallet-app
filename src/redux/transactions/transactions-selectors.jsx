50b65394d3a2590338df9d88bf012151434cf244
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-02-22 00:03:35 +0200

    commit fix

--- src/redux/transactions/transactions-selectors.jsx
+++ src/redux/transactions/transactions-selectors.jsx
@@ -1,5 +1,3 @@
 export const getAllTransactions = state => state.transactions.items;
-// export const getIncomeTransactions = state => state.transaction;
-// export const getExpensesTransactions = state => state.transaction;
 export const getBalance = state => state.transactions.balance;
 export const getLoading = state => state.transactions.loading;
