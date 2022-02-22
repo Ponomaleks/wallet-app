075312d213e8612c4aa3467c9aae4797de111e65
Roman Leshchenko <xetyri2014@gmail.com>
2022-02-22 22:53:02 +0200

    adds select and connection to the back

--- src/redux/operations/operations-operation.js
+++ src/redux/operations/operations-operation.js
@@ -3,93 +3,24 @@ import {
   addNewOperationRequest,
   addNewOperationSuccess,
   addNewOperationError,
-  getOperationsRequest,
-  getOperationsSuccess,
-  getOperationsError,
-  deleteOperationRequest,
-  deleteOperationError,
-  deleteOperationSuccess,
-  changeOperationSuccess,
+
 } from './operations-action';
-// import actions from '../statistics/statistics-actions';
-import { operationsAction } from '.';
 
-const getOperations = () => async dispatch => {
-  dispatch(getOperationsRequest());
-
-  try {
-    const {
-      data: {
-        response: { userOperations },
-      },
-    } = await axios.get('/api/operations');
-
-    dispatch(getOperationsSuccess(userOperations));
-  } catch (error) {
-    dispatch(getOperationsError(error.message));
-  }
-};
+axios.defaults.baseURL = 'http://localhost:8080/api';
 
 const createOperation = operation => async dispatch => {
   dispatch(addNewOperationRequest());
   try {
     const {
       data: {
-        data: { userOperations, totalBalance },
+        data: { userOperations},
       },
-    } = await axios.post('/api/operations', operation);
-
+    } = await axios.post('/transaction/add', operation);
+    
     dispatch(addNewOperationSuccess(userOperations));
-    // dispatch(actions.fetchBalanceSuccess(totalBalance));
   } catch (error) {
     dispatch(addNewOperationError(error.message));
   }
 };
 
-const changeOperation = (operation, operationId) => async dispatch => {
-  dispatch(operationsAction.changeOperationRequest());
-
-  try {
-    const {
-      data: {
-        data: { userOperations, totalBalance },
-      },
-    } = await axios.patch(`/api/operations/${operationId}`, operation);
-
-    dispatch(changeOperationSuccess(userOperations));
-    // dispatch(actions.fetchBalanceSuccess(totalBalance));
-  } catch (error) {
-    dispatch(operationsAction.changeOperationError(error));
-  }
-};
-
-const deleteOperation =
-  ({
-    deleteOperationId,
-    deleteOperationDate,
-    operationAmount,
-    deleteOperationType,
-  }) =>
-  async dispatch => {
-    dispatch(deleteOperationRequest());
-    try {
-      const {
-        data: {
-          data: { userOperations, totalBalance },
-        },
-      } = await axios.delete(`/api/operations/${deleteOperationId}`, {
-        data: {
-          date: deleteOperationDate,
-          type: deleteOperationType,
-          amount: operationAmount,
-        },
-      });
-
-      dispatch(deleteOperationSuccess(userOperations));
-    //   dispatch(actions.fetchBalanceSuccess(totalBalance));
-    } catch (error) {
-      dispatch(deleteOperationError(error.message));
-    }
-  };
-
-export { getOperations, createOperation, deleteOperation, changeOperation };
+export { createOperation};
