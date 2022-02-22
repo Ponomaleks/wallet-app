ea2707d3d043676c4efeb618f707437476551955
Alexander Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2022-02-22 19:58:38 +0200

    move token to localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,7 @@ import {
   PURGE,
   REGISTER,
 } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import storage from 'redux-persist/lib/storage/session';
 import { authReducer } from './auth';
 import transactionsReducer from './transactions';
 import { operationReducer } from './operations/operations-reducer';
