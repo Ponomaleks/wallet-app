ef1e56e8ea0f55b8e4ad145026eb314449324672
Roman Leshchenko <xetyri2014@gmail.com>
2022-02-22 02:02:11 +0200

    add form

--- src/components/ModalAddTransactions/Transactions/Transactions.jsx
+++ src/components/ModalAddTransactions/Transactions/Transactions.jsx
@@ -1,168 +1,111 @@
-import React, { useCallback } from 'react';
-import * as Yup from 'yup';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { useFormik } from 'formik';
-import { isEmpty } from 'lodash';
-// import { TextField } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import * as yup from 'yup';
 import s from './Transactions.module.css';
 import ButtonSwitch from '../ButtonSwitch/ButtonSwitch';
-import Buttons from '../Buttons/Buttons';
-import SelectIncome from '../SelectIncome/SelectIncome';
-import SelectExpense from '../SelectExpense/SelectExpense';
-import LocalizedDatePicker from '../DatePicker/DatePicker';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 import svg from '../../../images/modal-close-icon.svg';
-import {
-  operationsOperation,
-  operationsAction,
-} from '../../../redux/operations';
 
-const operationSchema = Yup.object({
-  amount: Yup.number('Enter your amount').required('Amount is required'),
-  comments: Yup.string('Enter your comments for operation')
-    .min(5, 'Your comments to short')
-    .max(30, 'Your comments to long'),
-  checked: Yup.bool(),
-  category: Yup.string('Choise your category outlay').when('checked', {
-    is: true,
-    then: Yup.string().required('Category is required'),
-  }),
-});
+import DataPicker from '../DatePicker/DatePicker';
 
-///////////////////////////////////////
+
+
 
 const Transactions = props => {
-  const dispatch = useDispatch();
+    
 
-  const formik = useFormik({
-    initialValues: {
-      type: props.operationType || '',
-      category: props.operationCategory || '',
-      amount:
-        props.operationType === 'outlay'
-          ? Number(props.operationAmount?.toString().slice(1))
-          : props.operationAmount || '',
-      date: props.operationDate ? new Date(props.operationDate) : new Date(),
-      comments: props.operationComments || '',
-      checked: isChecked(props),
-      selectError: '',
-      operationId: props.operationId || '',
-    },
-    validationSchema: operationSchema,
+    const validationSchema = yup.object({
+        amount: yup
+            .number('Enter your amount')
+            .required('Amount is required'),
+        commentary: yup
+            .string('Enter your comments for operation')
+            .min(5, 'Your comments to short')
+            .max(30, 'Your comments to long'),
+    });
 
-    onSubmit: (values, { resetForm }) => {
-      onFormSubmit(values, resetForm);
-    },
-  });
-
-  function isChecked(props) {
-    if (isEmpty(props) || props?.operationType === 'outlay') {
-      return true;
-    }
-
-    return false;
-  }
-
-  const closeModal = useCallback(
-    () => dispatch(operationsAction.closeModal()),
-    [dispatch],
-  );
-
-  const closeChangeOperationModal = useCallback(
-    () => dispatch(operationsAction.modalEditOperation()),
-    [dispatch],
-  );
-
-  function onFormSubmit(values, resetForm) {
-    let type = '';
-    if (values.checked) {
-      type = 'outlay';
-    } else {
-      type = 'income';
-    }
-
-    const dateSeconds = values.date.getMilliseconds();
-    const dateMinutes = values.date.getMinutes();
-
-    values.date.setMilliseconds(dateSeconds);
-    values.date.setMinutes(dateMinutes);
-
-    let newOperation = {
-      type,
-      amount: values.amount,
-      date: Date.parse(values.date),
-      comments: values.comments,
-    };
-
-    if (!isEmpty(props)) {
-      if (values.category) {
-        newOperation = { ...newOperation, category: values.category };
-
-        dispatch(
-          operationsOperation.changeOperation(newOperation, values.operationId),
-        );
-
-        resetForm();
-        closeChangeOperationModal();
-        return;
-      }
-
-      dispatch(
-        operationsOperation.changeOperation(newOperation, values.operationId),
-      );
-
-      resetForm();
-      closeChangeOperationModal();
-      return;
-    }
-
-    if (values.category) {
-      if (values.category.includes('Add')) {
-        newOperation = {
-          ...newOperation,
-          category: values.category.slice(5, -1),
-        };
-
-        dispatch(operationsOperation.createOperation(newOperation));
-        resetForm();
-        closeModal();
-
-        return;
-      }
-
-      newOperation = { ...newOperation, category: values.category };
-    }
-
-    dispatch(operationsOperation.createOperation(newOperation));
-
-    resetForm();
-    closeModal();
-  }
-
-  return (
+/////////////////////////////////////////////////////////////
+    const formik = useFormik({
+        initialValues: {
+        amount: ' ',
+        commentary: ' ',
+        date: props.operationDate ? new Date(props.operationDate) : new Date(),
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+        console.log(values);
+        alert(JSON.stringify(values, null, 2));
+        console.log(values);
+        },
+    });
+    
+    return(
     <>
-      <button className={s.closeButton} type="button" onClick={closeModal}>
+        <button className={s.closeButton} type="button" onClick={console.log("ok")}>
         <img src={svg} alt="" />
-      </button>
-      <div>
-        <form className={s.form} onSubmit={formik.handleSubmit}>
-          <h2 className={s.leader}>Add transaction</h2>
+        </button>
+        <div>
+            <form onSubmit={formik.handleSubmit}>
+                <h2 className={s.leader}>Add transaction</h2>
+                <ButtonSwitch
+                name="checked"
+                value={formik.values.checked}
+                changeSwitch={formik.setFieldValue}
+                />
+                <TextField
+                fullWidth
+                id="amount"
+                name="amount"
+                type="number"
+                label="Amount"
+                value={formik.values.amount}
+                onChange={formik.handleChange}
+                error={formik.touched.amount && Boolean(formik.errors.amount)}
+                helperText={formik.touched.amount && formik.errors.amount}
+                />
 
-          <ButtonSwitch
-            name="checked"
-            value={formik.values.checked}
-            changeSwitch={formik.setFieldValue}
-          />
+                <DataPicker
+                name="date"
+                value={formik.values.date}
+                onChange={formik.setFieldValue}
+                />
 
-          {formik.values.checked ? <SelectExpense /> : <SelectIncome />}
+                <TextField
+                fullWidth
+                id="commentary"
+                name="commentary"
+                type="text"
+                label="Commentary"
+                value={formik.values.commentary}
+                onChange={formik.handleChange}
+                error={formik.touched.commentary && Boolean(formik.errors.commentary)}
+                helperText={formik.touched.commentary && formik.errors.commentary}
+                />
+                <div className={s.buttonsContainer}>
+                <button
+                    type="submit"
+                    className={[s.buttons, s.buttonAddTransaction].join(' ')}
+                >
+                    <p>Add Transaction</p>
+                </button>
 
-          <LocalizedDatePicker />
-          <input name="sumTransaction" placeholder="0.00" type="number" />
-          <input name="commentTransaction" placeholder="Comment" type="text" />
-          <Buttons />
-        </form>
-      </div>
+                <button
+                    type="button"
+                    className={[s.buttons, s.buttonCancel].join(' ')}
+                    onClick={console.log("ok")}
+                >
+                    <p>Cancel</p>
+                </button>
+                </div>
+            </form>
+        </div>
     </>
-  );
-};
+    );
+
+}
 
 export default Transactions;
