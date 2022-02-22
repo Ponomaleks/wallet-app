5df8d1fff39a2d871b6c7e4c26832099e3d72d24
Alexander Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2022-02-22 04:47:53 +0200

    push

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -9,7 +9,7 @@ import Sidebar from '../../components/Sidebar';
 import s from './HomeView.module.css';
 import Header from '../../components/Header';
 import Modal from '../../components/ModalAddTransactions/Modal';
-
+import BackdropFilter from '../../components/BackdropFilter/BackdropFilter';
 // import { useSelector } from 'react-redux';
 // import { getIsLoggedIn } from '../redux/user';
 
@@ -19,36 +19,41 @@ const HomeView = () => {
   return (
     <>
       <Header />
-      <div className={`container ${s.homeView} ${s.homeBackground}`}>
-        {/* {!isLoggedIn && <div>Please, register or log in to start. </div>} */}
-        <Sidebar />
-        <Media
-          queries={{
-            mobile: '(max-width: 767px)',
-            other: '(min-width: 768px)',
-          }}
-        >
-          {matches => (
-            <Fragment>
-              {matches.mobile && (
-                <Box
-                  height="100vh"
-                  display="flex"
-                  flexDirection="column"
-                  alignItems="center"
-                  padding="0px"
-                >
-                  
-                  <TableTransactionsMobile />
-                </Box>
-              )}
-              {matches.other && <><Modal/><TableTransactions /></>}
-              
-            </Fragment>
-          )}
-        </Media>
-        <Modal />
-      </div>
+      <BackdropFilter>
+        <div className={`container ${s.homeView} ${s.homeBackground}`}>
+          {/* {!isLoggedIn && <div>Please, register or log in to start. </div>} */}
+          <Sidebar />
+          <Media
+            queries={{
+              mobile: '(max-width: 767px)',
+              other: '(min-width: 768px)',
+            }}
+          >
+            {matches => (
+              <Fragment>
+                {matches.mobile && (
+                  <Box
+                    height="100vh"
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                    padding="0px"
+                  >
+                    <TableTransactionsMobile />
+                  </Box>
+                )}
+                {matches.other && (
+                  <>
+                    <Modal />
+                    <TableTransactions />
+                  </>
+                )}
+              </Fragment>
+            )}
+          </Media>
+          <Modal />
+        </div>
+      </BackdropFilter>
     </>
   );
 };
